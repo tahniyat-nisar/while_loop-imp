26954e1f422a9c5ae820a74b44656662b305f3bf
tahniyat-nisar <93429497+tahniyat-nisar@users.noreply.github.com>
2021-10-30 16:37:49 +0530

    Add files via upload

---
+++ sum of even numbers 1 to n.py
@@ -0,0 +1,7 @@
+n=int(input("enter a number:"))
+i=2
+sum=0
+while i<=n:
+    sum=sum+i
+    print(sum)
+    i=i+2
---
+++ sum of even numbers n to1.py
@@ -0,0 +1,10 @@
+n=int(input("enter a number:"))
+sum=0
+while n>0:
+    if n%2==0:
+         sum=sum+n
+    print(sum)
+    n=n-1
+
+
+
