1e88beb0eadb14ade08c774b27d1d6d0e70a04b4
tahniyat-nisar <93429497+tahniyat-nisar@users.noreply.github.com>
2021-10-30 16:38:55 +0530

    Add files via upload

---
+++ sum of odd numbers 1 to n.py
@@ -0,0 +1,9 @@
+n=int(input("enter a number to get sum of odd numbers up to:"))
+i=1
+sum=0
+while i<=n:
+        sum=sum+i
+        i=i+2
+        print(sum)
+
+
---
+++ sum of odd numbers n to1.py
@@ -0,0 +1,7 @@
+n=int(input("enter a number:"))
+sum=0
+while n>=1:
+    sum=sum+n
+    print(sum)
+    n=n-2
+    
