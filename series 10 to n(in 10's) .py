1baebc6868ab4f2fea20c409379ffa9150348e0f
tahniyat-nisar <93429497+tahniyat-nisar@users.noreply.github.com>
2021-10-30 16:18:13 +0530

    Add files via upload

---
+++ series 10 to n(in 10's) .py
@@ -0,0 +1,6 @@
+n=int(input("enter a multiple of 10:"))
+i=10
+while i<=n:
+    if i%10==0:
+        print(i)
+        i=i+10
---
+++ series n(in 10's) to 10.py
@@ -0,0 +1,6 @@
+n=int(input("enter a mutiple of 10:"))
+while n>0:
+    if n%10==0:
+        print(n)
+        n=n-10
+        
